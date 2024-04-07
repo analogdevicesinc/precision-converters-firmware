5d5e99f9d832114ec0b355bb86e09daa714092d2
SGudla <Saikiran.Gudla@analog.com>
2024-04-07 12:21:37 +0530

    docs: Fix doc build warnings
    Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -19,7 +19,7 @@ extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'source/iio_osc/iio_osc.rst']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
