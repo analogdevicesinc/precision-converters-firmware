7a225bddc4ef91a539bfc09708128b45b3d72977
Janani Sunil <janani.sunil@analog.com>
2024-04-25 17:19:50 +0530

    projects:evb_discovery_firmware: Added support for STM32H5
    1) Added support for STM32H5 platform in the existing code
    
    2) Added app_config_stm32.h/app_config_stm32.c files
    
    3) Updated gitignore and added mbedignore
    
    4) Added a macro for unused device ID in the app_config_mbed.h
    
    5) Updated src.mk to exclude stm32 files for mbed build
    
    6) Updated copyright year in relevant files
    
    Signed-off-by: Janani Sunil <janani.sunil@analog.com>

--- projects/evb_discovery_firmware/src.mk
+++ projects/evb_discovery_firmware/src.mk
@@ -10,5 +10,12 @@ SRC_DIRS += $(LIBRARIES_PATH)/precision-converters-library/sdp_k1_sdram
 SRC_DIRS += $(LIBRARIES_PATH)/precision-converters-library/common
 SRC_DIRS += $(LIBRARIES_PATH)/precision-converters-library/board_info
 
+ifeq 'mbed' '$(PLATFORM)'
+# ALL_IGNORED_FILES variable used for excluding particular source files in SRC_DIRS in Build
+SRC_DIRS += $(LIBRARIES_PATH)/no-OS/drivers/platform/mbed
+ALL_IGNORED_FILES += $(PROJECT_APP_PATH)/app_config_stm32.c
+ALL_IGNORED_FILES += $(PROJECT_APP_PATH)/app_config_stm32.h
+
 # Extra Macros
 override NEW_CFLAGS += -DACTIVE_PLATFORM=MBED_PLATFORM
+endif
