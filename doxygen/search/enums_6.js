10541ba86ba7d9fee6aed09ef7256abdf619a831
Sai Kiran Gudla <118167755+SaikiranGudla@users.noreply.github.com>
2025-01-20 14:51:55 +0000

    deploy: 1b858b7c152c845610c917551ee665415bdc9de6

--- doxygen/search/enums_6.js
+++ doxygen/search/enums_6.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['range_5fe_0',['range_e',['../iio__ad7606_8c.html#a1d072e73707da78e148cc2f25af2bd87',1,'iio_ad7606.c']]],
   ['reference_5ftype_1',['reference_type',['../ad7124__console_2app_2ad7124__console__app_8h.html#aec9a2192206f2614938b98fd7fa8113e',1,'ad7124_console_app.h']]],
-  ['registers_5fe_2',['registers_e',['../adt7xxx__support_8h.html#a7496716d25746ab1958284dea9f5ff4f',1,'adt7xxx_support.h']]],
-  ['rtd_5f3wire_5fcalibration_5ftype_3',['rtd_3wire_calibration_type',['../ad7124__temperature-measure_2app_2ad7124__console__app_8c.html#abce3c0ffc447e5de1ee045f85b1487fc',1,'ad7124_console_app.c']]]
+  ['reg_5faccess_5fmode_2',['reg_access_mode',['../ad3530r__iio_8h.html#a1ac6da5d718cb7ac912e05a9ddd8880a',1,'ad3530r_iio.h']]],
+  ['registers_5fe_3',['registers_e',['../adt7xxx__support_8h.html#a7496716d25746ab1958284dea9f5ff4f',1,'adt7xxx_support.h']]],
+  ['rtd_5f3wire_5fcalibration_5ftype_4',['rtd_3wire_calibration_type',['../ad7124__temperature-measure_2app_2ad7124__console__app_8c.html#abce3c0ffc447e5de1ee045f85b1487fc',1,'ad7124_console_app.c']]]
 ];
