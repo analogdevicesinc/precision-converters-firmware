167a81720898e849b3f902365b6688b823bcee05
Kantipudi-Supriya <supriya.kantipudi@analog.com>
2025-04-10 10:26:43 +0530

    projects:ad7606:Added the stm support 1)Added the app_config_stm32.h and app_config_stm32.c file 2)Added the app_config_mbed.h and app_config_mbed.c file 3)Added the .extsettings and ad7606.ioc file 4)updated the src.mk file and added the .mbedignore file 5)updated the no-os commit id
    Signed-off-by: Kantipudi-Supriya <supriya.kantipudi@analog.com>

--- projects/ad7606_iio/app/app_config.h
+++ projects/ad7606_iio/app/app_config.h
@@ -2,7 +2,7 @@
  *   @file   app_config.h
  *   @brief  Header file for application configurations (platform-agnostic)
 ******************************************************************************
-* Copyright (c) 2020-2023 Analog Devices, Inc.
+* Copyright (c) 2020-2023, 2025 Analog Devices, Inc.
 * All rights reserved.
 *
 * This software is proprietary to Analog Devices, Inc. and its licensors.
@@ -25,6 +25,7 @@
 
 /* List of supported platforms*/
 #define	MBED_PLATFORM		1
+#define STM32_PLATFORM      2
 
 /* List of data capture modes */
 #define BURST_DATA_CAPTURE			0
@@ -36,7 +37,7 @@
 
 /* Select the active platform (default is Mbed) */
 #if !defined(ACTIVE_PLATFORM)
-#define ACTIVE_PLATFORM		MBED_PLATFORM
+#define ACTIVE_PLATFORM		STM32_PLATFORM
 #endif
 
 /* Select the ADC data capture mode (default is CC mode) */
@@ -45,7 +46,7 @@
 #endif
 
 /* Enable the UART/VirtualCOM port connection (default VCOM) */
-//#define USE_PHY_COM_PORT		// Uncomment to select UART
+#define USE_PHY_COM_PORT		// Uncomment to select UART
 
 #if !defined(USE_PHY_COM_PORT)
 #define USE_VIRTUAL_COM_PORT
@@ -58,7 +59,7 @@
  * The ACTIVE_DEVICE is default set to AD7606B if device type is not defined.
  * */
 
-//#define DEV_AD7606B
+#define DEV_AD7606C_16
 
 #if defined(DEV_AD7605_4)
 #define ACTIVE_DEVICE		ID_AD7605_4
@@ -139,18 +140,13 @@
 
 #if (ACTIVE_PLATFORM == MBED_PLATFORM)
 #include "app_config_mbed.h"
-
 #define HW_CARRIER_NAME		TARGET_NAME
-
 /* Redefine the init params structure mapping w.r.t. platform */
 #define pwm_extra_init_params mbed_pwm_extra_init_params
-#if defined(USE_VIRTUAL_COM_PORT)
-#define uart_extra_init_params mbed_vcom_extra_init_params
-#define uart_ops mbed_virtual_com_ops
-#else
+#define vcom_extra_init_params mbed_vcom_extra_init_params
+#define vcom_ops mbed_virtual_com_ops
 #define uart_extra_init_params mbed_uart_extra_init_params
 #define uart_ops mbed_uart_ops
-#endif
 #define spi_extra_init_params mbed_spi_extra_init_params
 #define i2c_extra_init_params mbed_i2c_extra_init_params
 #define trigger_gpio_irq_extra_params mbed_trigger_gpio_irq_init_params
@@ -168,15 +164,50 @@
 #define gpio_ops mbed_gpio_ops
 #define spi_ops mbed_spi_ops
 #define i2c_ops mbed_i2c_ops
+#define pwm_ops mbed_pwm_ops
 #define trigger_gpio_irq_ops mbed_gpio_irq_ops
 #define trigger_gpio_handle 0	// Unused macro
 #define IRQ_INT_ID GPIO_IRQ_ID1
 #define TRIGGER_GPIO_PORT 0  // Unused macro
 #define TRIGGER_GPIO_PIN  PWM_TRIGGER
 #define TRIGGER_INT_ID	GPIO_IRQ_ID1
+#elif (ACTIVE_PLATFORM == STM32_PLATFORM)
+#include "app_config_stm32.h"
+#define pwm_extra_init_params stm32_pwm_extra_init_params
+#define vcom_extra_init_params stm32_vcom_extra_init_params
+#define vcom_ops  stm32_usb_uart_ops
+#define uart_extra_init_params stm32_uart_extra_init_params
+#define uart_ops stm32_uart_ops
+#define spi_extra_init_params stm32_spi_extra_init_params
+#define i2c_extra_init_params stm32_i2c_extra_init_params
+#define trigger_gpio_irq_extra_params stm32_trigger_gpio_irq_init_params
+#define trigger_gpio_extra_init_params stm32_trigger_gpio_extra_init_params
+#define reset_gpio_extra_init_params stm32_reset_gpio_extra_init_params
+#define convst_gpio_extra_init_params stm32_convst_gpio_extra_init_params
+#define busy_gpio_extra_init_params stm32_busy_gpio_extra_init_params
+#define osr0_gpio_extra_init_params stm32_osr0_gpio_extra_init_params
+#define osr1_gpio_extra_init_params stm32_osr1_gpio_extra_init_params
+#define osr2_gpio_extra_init_params stm32_osr2_gpio_extra_init_params
+#define range_gpio_extra_init_params stm32_range_gpio_extra_init_params
+#define stdby_gpio_extra_init_params stm32_stdby_gpio_extra_init_params
+#define pwm_gpio_extra_init_params  stm32_pwm_gpio_extra_init_params
+#define trigger_gpio_ops stm32_gpio_ops
+#define irq_ops stm32_gpio_irq_ops
+#define gpio_ops stm32_gpio_ops
+#define spi_ops stm32_spi_ops
+#define i2c_ops stm32_i2c_ops
+#define pwm_ops stm32_pwm_ops
+#define trigger_gpio_irq_ops stm32_gpio_irq_ops
+#define trigger_gpio_handle 0	// Unused macro
+#define TRIGGER_GPIO_PIN  IRQ_INT_ID
+#define TRIGGER_INT_ID	IRQ_INT_ID
 #else
 #error "No/Invalid active platform selected"
 #endif
+
+/* PWM period and duty cycle */
+#define CONV_TRIGGER_PERIOD_NSEC		(((float)(1.0 / SAMPLING_RATE) * 1000000) * 1000)
+#define CONV_TRIGGER_DUTY_CYCLE_NSEC	(CONV_TRIGGER_PERIOD_NSEC / 2)
 
 /* ADC max count (full scale value) for unipolar inputs */
 #define ADC_MAX_COUNT_UNIPOLAR	(uint32_t)((1 << AD7606X_ADC_RESOLUTION) - 1)
@@ -202,6 +233,17 @@
 
 #if !defined(PLATFORM_NAME)
 #define PLATFORM_NAME	HW_CARRIER_NAME
+#endif
+
+/* Check if any serial port available for use as console stdio port */
+#if defined(USE_PHY_COM_PORT)
+/* If PHY com is selected, VCOM or alternate PHY com port can act as a console stdio port */
+#if (ACTIVE_PLATFORM == MBED_PLATFORM || ACTIVE_PLATFORM == STM32_PLATFORM)
+#define CONSOLE_STDIO_PORT_AVAILABLE
+#endif
+#else
+/* If VCOM is selected, PHY com port will/should act as a console stdio port */
+#define CONSOLE_STDIO_PORT_AVAILABLE
 #endif
 
 /* Below USB configurations (VID and PID) are owned and assigned by ADI.
