27b9f4970e65de705cf776703c4581816276f80d
Kantipudi-Supriya <supriya.kantipudi@analog.com>
2025-06-20 11:07:49 +0530

    projects:ad7191:Add ad7191 firmware support
    Signed-off-by: Kantipudi-Supriya <supriya.kantipudi@analog.com>

---
+++ projects/ad7191_iio/app/app_config.c
@@ -0,0 +1,182 @@
+/***************************************************************************//**
+ *   @file    app_config.c
+ *   @brief   Application configurations module
+ *   @details This module contains the configurations needed for ad7191
+ *            IIO application firmware
+********************************************************************************
+ * Copyright (c) 2024 Analog Devices, Inc.
+ * All rights reserved.
+ *
+ * This software is proprietary to Analog Devices, Inc. and its licensors.
+ * By using this software you agree to the terms of the associated
+ * Analog Devices Software License Agreement.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "app_config.h"
+#include "common.h"
+#include "no_os_error.h"
+#include "no_os_uart.h"
+#include "no_os_gpio.h"
+
+/******************************************************************************/
+/************************ Macros/Constants ************************************/
+/******************************************************************************/
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+/* UART init parameters structure */
+struct no_os_uart_init_param uart_init_params = {
+	.device_id = 0,
+	.baud_rate = IIO_UART_BAUD_RATE,
+	.size = NO_OS_UART_CS_8,
+	.parity = NO_OS_UART_PAR_NO,
+	.stop = NO_OS_UART_STOP_1_BIT,
+#if (ACTIVE_PLATFORM == STM32_PLATFORM)
+	.asynchronous_rx = true,
+	.irq_id = UART_IRQ_ID,
+#endif
+	.platform_ops = &uart_ops,
+	.extra = &uart_extra_init_params
+};
+
+/* I2C init parameters */
+static struct no_os_i2c_init_param no_os_i2c_init_params = {
+	.device_id = I2C_DEVICE_ID,
+	.platform_ops = &i2c_ops,
+	.max_speed_hz = 100000,
+#if(ACTIVE_PLATFORM == MBED_PLATFORM)
+	.extra = &i2c_extra_init_params
+#endif
+};
+
+/* EEPROM init parameters */
+static struct eeprom_24xx32a_init_param eeprom_extra_init_params = {
+	.i2c_init = &no_os_i2c_init_params
+};
+
+/* EEPROM init parameters */
+static struct no_os_eeprom_init_param eeprom_init_params = {
+	.device_id = 0,
+	.platform_ops = &eeprom_24xx32a_ops,
+	.extra = &eeprom_extra_init_params
+};
+
+/* External interrupt init parameters */
+static struct no_os_irq_init_param trigger_gpio_irq_params = {
+	.irq_ctrl_id = RDY_PIN,
+	.platform_ops = &trigger_gpio_irq_ops,
+	.extra = &trigger_gpio_irq_extra_params
+};
+
+/* External interrupt callback descriptor */
+static struct no_os_callback_desc ext_int_callback_desc = {
+	.callback = data_capture_callback,
+	.event = NO_OS_EVT_GPIO,
+	.peripheral = NO_OS_GPIO_IRQ,
+};
+
+/* UART descriptor */
+struct no_os_uart_desc *uart_desc;
+
+/* EEPROM descriptor */
+struct no_os_eeprom_desc *eeprom_desc;
+
+/* External interrupt descriptor */
+struct no_os_irq_ctrl_desc *trigger_irq_desc;
+
+/******************************************************************************/
+/************************ Functions Prototypes ********************************/
+/******************************************************************************/
+
+/**
+ * @brief 	Initialize the UART peripheral
+ * @return	0 in case of success, negative error code otherwise.
+ */
+static int32_t init_uart(void)
+{
+	int32_t ret;
+
+	ret = no_os_uart_init(&uart_desc, &uart_init_params);
+	if (ret) {
+		return ret;
+	}
+
+	return 0;
+}
+
+/**
+ * @brief 	Initialize the IRQ contoller
+ * @return	0 in case of success, negative error code otherwise.
+ * @details	This function initialize the interrupts for system peripherals
+ */
+static int32_t init_interrupt(void)
+{
+	int32_t ret;
+
+	/* Init interrupt controller for external interrupt (for monitoring
+		* data Ready event on RDY pin) */
+	ret = no_os_irq_ctrl_init(&trigger_irq_desc, &trigger_gpio_irq_params);
+	if (ret) {
+		return ret;
+	}
+
+#if (DATA_CAPTURE_MODE == BURST_DATA_CAPTURE)
+	/* Register a callback function for external interrupt */
+	ret = no_os_irq_register_callback(trigger_irq_desc,
+					  IRQ_INT_ID, &ext_int_callback_desc);
+	if (ret) {
+		return ret;
+	}
+
+	ret = no_os_irq_trigger_level_set(trigger_irq_desc,
+					  IRQ_INT_ID, NO_OS_IRQ_EDGE_FALLING);
+	if (ret) {
+		return ret;
+	}
+#endif
+
+	return 0;
+}
+
+/**
+ * @brief 	Initializing system peripherals
+ * @return	0 in case of success, negative error code otherwise.
+ * @details	This function initializes system peripherals for the application
+ */
+int32_t init_system(void)
+{
+	int32_t ret;
+
+#if (ACTIVE_PLATFORM == STM32_PLATFORM)
+	stm32_system_init();
+#endif
+
+	ret = init_uart();
+	if (ret) {
+		return ret;
+	}
+
+	ret = init_interrupt();
+	if (ret) {
+		return ret;
+	}
+
+#if defined(USE_SDRAM)
+	ret = sdram_init();
+	if (ret) {
+		return ret;
+	}
+#endif
+
+	/* Uncomment it out after testing */
+//	ret = eeprom_init(&eeprom_desc, &eeprom_init_params);
+//	if (ret) {
+//		return ret;
+//	}
+
+	return 0;
+}
