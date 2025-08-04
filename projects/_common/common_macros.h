58f61d1b22586ebea3d64be5ec2918cbafde40a8
Kantipudi-Supriya <supriya.kantipudi@analog.com>
2025-08-04 14:57:29 +0530

    project:Added common_macros.h file 1)Added common_macros.h file to _common folder
    Signed-off-by: Kantipudi-Supriya <supriya.kantipudi@analog.com>

---
+++ projects/_common/common_macros.h
@@ -0,0 +1,23 @@
+/***************************************************************************************************//**
+ *   @file   common_macros.h
+     @brief  The file contain common macros that needs to be followed same across all the projects
+********************************************************************************************************
+* Copyright (c) 2025 Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* This software is proprietary to Analog Devices, Inc. and its licensors.
+* By using this software you agree to the terms of the associated
+* Analog Devices Software License Agreement.
+**********************************************************************************************************/
+#ifndef COMMON_MACROS_H
+#define COMMON_MACROS_H
+
+/* 
+ * List of supported platforms.
+ * One of these platforms is selected as the ACTIVE_PLATFORM in the app_config.h file of every project.
+ */
+#define STM32_PLATFORM      2
+
+#endif /* COMMON_MACROS_H */
+
