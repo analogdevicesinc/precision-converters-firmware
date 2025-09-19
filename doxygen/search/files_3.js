83735b1d4c3936d8475b4059cadc2a65506f9234
Sai Kiran Gudla <118167755+SaikiranGudla@users.noreply.github.com>
2025-09-19 10:01:26 +0000

    deploy: 96f29f30bcb7bb05818b2227360e3b2bd91197ff

--- doxygen/search/files_3.js
+++ doxygen/search/files_3.js
@@ -3,5 +3,6 @@ var searchData=
   ['cn0586_5fsupport_2ec_0',['cn0586_support.c',['../cn0586__support_8c.html',1,'']]],
   ['cn0586_5fsupport_2eh_1',['cn0586_support.h',['../cn0586__support_8h.html',1,'']]],
   ['common_2ec_2',['common.c',['../common_8c.html',1,'']]],
-  ['common_2eh_3',['common.h',['../common_8h.html',1,'']]]
+  ['common_2eh_3',['common.h',['../common_8h.html',1,'']]],
+  ['common_5fmacros_2eh_4',['common_macros.h',['../common__macros_8h.html',1,'']]]
 ];
