742b152c364b52eb704bfbb4df1566db827c07f9
SGudla <Saikiran.Gudla@analog.com>
2025-10-13 16:21:31 +0530

    projects: ad5710r_iio: Release AD5710R IIO project
    Add AD5710R IIO project.
    
    Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

---
+++ projects/ad5710r_iio/app/version.h
@@ -0,0 +1,34 @@
+/*************************************************************************//**
+ *   @file   version.h
+ *   @brief  Version macros for AD5710R IIO Firmware
+******************************************************************************
+* Copyright (c) 2025 Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* This software is proprietary to Analog Devices, Inc. and its licensors.
+* By using this software you agree to the terms of the associated
+* Analog Devices Software License Agreement.
+*****************************************************************************/
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#include "adi_version.h"
+
+/******************************************************************************/
+/* Define firmware_version string */
+#define MAJOR_VERSION       1
+#define MINOR_VERSION       0
+#define PATCH_VERSION       0
+#define QUALITY_LEVEL       QUALITY_LEVEL_RC
+#define STATE_VERSION       0
+
+/* Create firmware_version string */
+#define FIRMWARE_VERSION ADI_CONSTRUCT_VERSION(MAJOR_VERSION, \
+                                               MINOR_VERSION, \
+                                               PATCH_VERSION, \
+                                               QUALITY_LEVEL, \
+                                               STATE_VERSION)
+
+#endif //VERSION_H
