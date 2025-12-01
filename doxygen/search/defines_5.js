f6452768b4668607cf45c01e0defe45c1524f093
Disha D <disha.d@analog.com>
2025-12-01 09:30:01 +0000

    deploy: f28172ab6e54fa4a24f048d0400796680d3abd74

--- doxygen/search/defines_5.js
+++ doxygen/search/defines_5.js
@@ -1,18 +1,17 @@
 var searchData=
 [
-  ['ecg_5fconfig_0',['ECG_CONFIG',['../ad4130__iio_2app_2app__config_8h.html#aceb2f41577dc10a06ef684019be269cd',1,'app_config.h']]],
-  ['eeprom_5fdev_5faddr_5fend_1',['EEPROM_DEV_ADDR_END',['../common_8h.html#a5742361d9ed0435ba0e0c4c1ae4e7bdc',1,'common.h']]],
-  ['eeprom_5fdev_5faddr_5fstart_2',['EEPROM_DEV_ADDR_START',['../common_8h.html#ab9f70d625bf38d02c1bbca4c6b6e537b',1,'common.h']]],
-  ['eeprom_5fops_5fstart_5fdelay_3',['eeprom_ops_start_delay',['../ad4130__iio_2app_2app__config_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;app_config.c'],['../ad7124__iio_2app_2app__config_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;app_config.c'],['../common_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;common.c']]],
-  ['enable_4',['ENABLE',['../ad77681_8h.html#a514ad415fb6125ba296793df7d1a468a',1,'ad77681.h']]],
-  ['end_5fof_5fchn_5',['END_OF_CHN',['../ad7689__iio_8c.html#abe0ebd4514a2e953cfeeed50e0de72a4',1,'ad7689_iio.c']]],
-  ['eol_6',['eol',['../ad5933__console_2app_2main_8c.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;main.c'],['../adt7420__console_2app_2app__config_8h.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;app_config.h'],['../adi__console__menu_8h.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;adi_console_menu.h']]],
-  ['escape_5fkey_5fcode_7',['ESCAPE_KEY_CODE',['../adi__console__menu_8h.html#a69d95ec52e5394a000ac8f3dbf87e42a',1,'adi_console_menu.h']]],
-  ['exit_5fcont_5fread_8',['EXIT_CONT_READ',['../ad77681_8h.html#affc573d91864e9bf1a21e4326b263ba7',1,'ad77681.h']]],
-  ['ext_5fi2c_5faddress_9',['EXT_I2C_ADDRESS',['../adt7420__console_2app_2app__config_8h.html#ab86cff21388a640eae3ebb450fec3393',1,'app_config.h']]],
-  ['ext_5fint_5fextra_5finit_5fparams_10',['ext_int_extra_init_params',['../ad355xr__iio_2app_2app__config__mbed_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config_mbed.h'],['../ad355xr__iio_2app_2app__config__stm32_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config_stm32.h'],['../ad4130__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad4134__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad7124__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad717x__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad77681__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h']]],
-  ['ext_5fmclk_5fpin_11',['EXT_MCLK_PIN',['../ad777x__iio_2app_2app__config__mbed_8h.html#a378319394662ca6a43436cdd584786c3',1,'app_config_mbed.h']]],
-  ['external_12',['EXTERNAL',['../ad717x__menu__defines_8h.html#af3fe37c1cda80aa7202b5a3bb7557dc9',1,'ad717x_menu_defines.h']]],
-  ['external_5fvref_5fsource_13',['EXTERNAL_VREF_SOURCE',['../nanodac__console__app_8c.html#aa754e738eaf45e4c6d8b53972ea03b17',1,'nanodac_console_app.c']]],
-  ['external_5fvref_5fvoltage_14',['EXTERNAL_VREF_VOLTAGE',['../ad5592r__console__app_8c.html#aeaf59fd28609dc7c39b597c87bea8587',1,'ad5592r_console_app.c']]]
+  ['eeprom_5fdev_5faddr_5fend_0',['EEPROM_DEV_ADDR_END',['../common_8h.html#a5742361d9ed0435ba0e0c4c1ae4e7bdc',1,'common.h']]],
+  ['eeprom_5fdev_5faddr_5fstart_1',['EEPROM_DEV_ADDR_START',['../common_8h.html#ab9f70d625bf38d02c1bbca4c6b6e537b',1,'common.h']]],
+  ['eeprom_5fops_5fstart_5fdelay_2',['eeprom_ops_start_delay',['../ad4130__iio_2app_2app__config_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;app_config.c'],['../ad7124__iio_2app_2app__config_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;app_config.c'],['../common_8c.html#ab30640ae1de3af90795143ee44f0cc65',1,'EEPROM_OPS_START_DELAY:&#160;common.c']]],
+  ['enable_3',['ENABLE',['../ad77681_8h.html#a514ad415fb6125ba296793df7d1a468a',1,'ad77681.h']]],
+  ['end_5fof_5fchn_4',['END_OF_CHN',['../ad7689__iio_8c.html#abe0ebd4514a2e953cfeeed50e0de72a4',1,'ad7689_iio.c']]],
+  ['eol_5',['eol',['../ad5933__console_2app_2main_8c.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;main.c'],['../adt7420__console_2app_2app__config_8h.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;app_config.h'],['../adi__console__menu_8h.html#a4e67e9429d48a2ba8f833ee3b1dceb5d',1,'EOL:&#160;adi_console_menu.h']]],
+  ['escape_5fkey_5fcode_6',['ESCAPE_KEY_CODE',['../adi__console__menu_8h.html#a69d95ec52e5394a000ac8f3dbf87e42a',1,'adi_console_menu.h']]],
+  ['exit_5fcont_5fread_7',['EXIT_CONT_READ',['../ad77681_8h.html#affc573d91864e9bf1a21e4326b263ba7',1,'ad77681.h']]],
+  ['ext_5fi2c_5faddress_8',['EXT_I2C_ADDRESS',['../adt7420__console_2app_2app__config_8h.html#ab86cff21388a640eae3ebb450fec3393',1,'app_config.h']]],
+  ['ext_5fint_5fextra_5finit_5fparams_9',['ext_int_extra_init_params',['../ad355xr__iio_2app_2app__config__mbed_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config_mbed.h'],['../ad355xr__iio_2app_2app__config__stm32_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config_stm32.h'],['../ad4134__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad7124__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad717x__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h'],['../ad77681__iio_2app_2app__config_8h.html#a1f771bc4cf07db397e9f94e0bf0e8836',1,'ext_int_extra_init_params:&#160;app_config.h']]],
+  ['ext_5fmclk_5fpin_10',['EXT_MCLK_PIN',['../ad777x__iio_2app_2app__config__mbed_8h.html#a378319394662ca6a43436cdd584786c3',1,'app_config_mbed.h']]],
+  ['external_11',['EXTERNAL',['../ad717x__menu__defines_8h.html#af3fe37c1cda80aa7202b5a3bb7557dc9',1,'ad717x_menu_defines.h']]],
+  ['external_5fvref_5fsource_12',['EXTERNAL_VREF_SOURCE',['../nanodac__console__app_8c.html#aa754e738eaf45e4c6d8b53972ea03b17',1,'nanodac_console_app.c']]],
+  ['external_5fvref_5fvoltage_13',['EXTERNAL_VREF_VOLTAGE',['../ad5592r__console__app_8c.html#aeaf59fd28609dc7c39b597c87bea8587',1,'ad5592r_console_app.c']]]
 ];
