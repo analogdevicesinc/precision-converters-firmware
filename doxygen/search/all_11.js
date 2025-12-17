2665b1429fce60ea76624fe316bcc53cd3b88b83
Disha D <disha.d@analog.com>
2025-12-17 09:36:26 +0000

    deploy: 6ba1af6769965ae8a444520044a60b32fb4792f3

--- doxygen/search/all_11.js
+++ doxygen/search/all_11.js
@@ -6,7 +6,7 @@ var searchData=
   ['qspi_5fdma_5firq_3',['QSPI_DMA_IRQ',['../ad4080__iio_2app_2app__config__stm32_8h.html#acf149680671909ec2df472b1523f49b7',1,'app_config_stm32.h']]],
   ['qspi_5fdma_5fnum_5fch_4',['QSPI_DMA_NUM_CH',['../ad4080__iio_2app_2app__config__stm32_8h.html#a95b580e8d07ebd0035fa00ee0ba8e754',1,'app_config_stm32.h']]],
   ['qspi_5fspeed_5',['QSPI_SPEED',['../ad4080__iio_2app_2app__config__stm32_8h.html#a5cecd92063821fed1ddc15d6e9e1f361',1,'app_config_stm32.h']]],
-  ['quality_5flevel_6',['quality_level',['../ad405x__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h'],['../ad4080__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h'],['../ad5710r__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h']]],
+  ['quality_5flevel_6',['quality_level',['../ad4080__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h'],['../ltc2672__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h'],['../ad5710r__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h'],['../ad405x__iio_2app_2version_8h.html#a1bee46a219fdaf5e494626114cf2a072',1,'QUALITY_LEVEL:&#160;version.h']]],
   ['quality_5flevel_5falpha_7',['QUALITY_LEVEL_ALPHA',['../adi__version_8h.html#a7a2ef878b95f87ab6d47d69de05868b7',1,'adi_version.h']]],
   ['quality_5flevel_5fbeta_8',['QUALITY_LEVEL_BETA',['../adi__version_8h.html#a74748dd7c096f53f53f77407dc478eb4',1,'adi_version.h']]],
   ['quality_5flevel_5fdev_9',['QUALITY_LEVEL_DEV',['../adi__version_8h.html#ac364811ae890cf87a2f3c573e044f866',1,'adi_version.h']]],
